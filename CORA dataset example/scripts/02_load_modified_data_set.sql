6537c50a7f79f9f069e997c73497cb85b2d4ea05
Karin Patenge <karin.patenge@oracle.com>
2023-12-08 11:24:35 +0100

    Data and scripts updated

---
+++ CORA dataset example/scripts/02_load_modified_data_set.sql
@@ -0,0 +1,50 @@
+--------------------------------------------
+-- Connect to your Oracle Database instance 
+-- using sqlcl with your DB user credentials
+--------------------------------------------
+
+set loadformat csv
+
+-- Import data using LOAD
+load table cora_cites CORA_CITES_DATA_TABLE.csv
+load table cora_content CORA_CONTENT_DATA_TABLE.csv
+
+/* Expected result:
+
+Load data into table CORAUSER.CORA_CITES
+
+csv
+column_names on
+delimiter ,
+enclosures ""
+double off
+encoding UTF8
+row_limit off
+row_terminator default
+skip_rows 0
+skip_after_names
+
+#INFO Number of rows processed: 5.429
+#INFO Number of rows in error: 0
+#INFO Last row processed in final committed batch: 5.429
+SUCCESS: Processed without errors
+
+Load data into table CORAUSER.CORA_CONTENT
+
+csv
+column_names on
+delimiter ,
+enclosures ""
+double off
+encoding UTF8
+row_limit off
+row_terminator default
+skip_rows 0
+skip_after_names
+
+#INFO Number of rows processed: 2.708
+#INFO Number of rows in error: 0
+#INFO Last row processed in final committed batch: 2.708
+SUCCESS: Processed without errors
+
+*/
---
+++ CORA dataset example/scripts/99_drop_data_set_tables.sql
@@ -0,0 +1,7 @@
+-------------------
+--  Clean up tables
+-------------------
+drop table cora_content purge
+/
+drop table cora_cites purge
+/
