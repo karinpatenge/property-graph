42ba05c954c57e1ad3445bdf897455755644368a
Karin Patenge <karin.patenge@oracle.com>
2023-12-19 14:28:29 +0100

    Scripts updated

--- Banking dataset example/scripts/database/02_load_data_set.sql
+++ Banking dataset example/scripts/database/02_load_data_set.sql
@@ -12,76 +12,41 @@
 --------------------------------------------
 
 set loadformat csv
+-- Make sure to have the decimals in the data correctly interpreted
 set load locale American America
 
 -- Import data using LOAD
 load table bank_accounts BANK_ACCOUNTS.csv
-load table bank_txns_temp BANK_TXNS.csv new
+load table bank_txns_temp BANK_TXNS.csv
 
-/* Expected result:
+/* Expected results:
 
-Load data into table GRAPHUSER.BANK_TXNS
-
-csv
-column_names on
-delimiter ,
-enclosures ""
-double off
-encoding UTF8
-row_limit off
-row_terminator default
-skip_rows 0
-skip_after_names
-
-#ERROR Omitted columns must be nullable
-#INFO Number of rows processed: 0
+Load data into table GRAPHUSER.BANK_ACCOUNTS
+#INFO Number of rows processed: 10.000
 #INFO Number of rows in error: 0
-#INFO No rows committed
-SEVERE: Load terminated
+#INFO Last row processed in final committed batch: 10.000
+SUCCESS: Processed without errors
 
-Create new table and load data into table GRAPHUSER.BANK_TXNS_TEMP
-
-csv
-column_names on
-delimiter ,
-enclosures ""
-double off
-encoding UTF8
-row_limit off
-row_terminator default
-skip_rows 0
-skip_after_names
-
-#INFO COLUMN 1: from_acct_id => FROM_ACCT_ID
-#INFO COLUMN 2: to_acct_id => TO_ACCT_ID
-#INFO COLUMN 3: txn_date => TXN_DATE
-#INFO COLUMN 4: amount => AMOUNT
-#INFO COLUMN 5: Currency => CURRENCY
-#INFO TIMESTAMP format detected: YYYY-MM-DD HH24:MI:SS.FF
-
-
-CREATE TABLE GRAPHUSER.BANK_TXNS_TEMP
- (
-  FROM_ACCT_ID VARCHAR2(26),
-  TO_ACCT_ID VARCHAR2(26),
-  TXN_DATE TIMESTAMP,
-  AMOUNT NUMBER(9),
-  CURRENCY VARCHAR2(26)
- )
-;
-
-#INFO Table created
+Load data into table GRAPHUSER.BANK_TXNS_TEMP
 #INFO Number of rows processed: 50.000
 #INFO Number of rows in error: 0
 #INFO Last row processed in final committed batch: 50.000
 SUCCESS: Processed without errors
+
 */
+
+-- Verify transaction data in the staging table
+select * from bank_txns_temp fetch first 10 rows only;
 
 -- Copy data from staging table BANK_TXNS_TEMP into BANK_TXNS
 insert into bank_txns (from_acct_id, to_acct_id, txn_date, amount, currency)(
     select from_acct_id, to_acct_id, txn_date, amount, currency from bank_txns_temp);
 
 commit;
+
+-- Verify that your data was loaded correctly
+select * from bank_accounts fetch first 10 rows only;
+select * from bank_txns fetch first 10 rows only;
 
 -- Drop staging table BANK_TXNS_TEMP
 drop table bank_txns_temp purge;
